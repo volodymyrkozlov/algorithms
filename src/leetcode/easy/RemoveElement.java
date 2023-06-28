bd9383c3d335a3e711a8e2f8e8a13dc2d03eb9a4
Volodymyr Kozlov <volodymyr.kozlow@gmail.com>
2023-06-28 14:41:06 +0200

    Added leet code easy RemoveElement solution

---
+++ src/leetcode/easy/RemoveElement.java
@@ -0,0 +1,18 @@
+package leetcode.easy;
+
+public class RemoveElement {
+    public static void main(String[] args) {
+        System.out.println(removeElement(new int[]{0, 1, 2, 2, 3, 0, 4, 2}, 2));
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+}
